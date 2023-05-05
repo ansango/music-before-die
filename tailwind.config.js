5dddc9d70470cec2b38158422d584e65a31803a6
Anibal Santos <anibalsantosgo@gmail.com>
2023-05-05 22:43:47 +0200

    feat(config): basic config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,90 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--sans)", ...fontFamily.sans],
+        serif: ["var(--serif)", ...fontFamily.serif],
+        display: ["var(--display)", ...fontFamily.sans],
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+      textColor: {
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
+        button: "var(--color-text-button)",
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        offset: "var(--color-background-offset)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
+        offset: "var(--color-border-offset)",
+      },
+      aspectRatio: {
+        "4/3": "4 / 3",
+        "4/5": "4 / 5",
+        "5/4": "5 / 4",
+        "9/16": "9 / 16",
+        "2/3": "2 / 3",
+        "3/2": "3 / 2",
+      },
+      opacity: {
+        7: ".075",
+        15: ".15",
+      },
+      maxWidth: {
+        "8xl": "86rem",
+      },
+      spacing: {
+        128: "32rem",
+      },
+      zIndex: {
+        "-1": "-1",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require("tailwindcss-debug-screens"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("tailwindcss-fluid-type")({
+      settings: {
+        fontSizeMin: 1.125,
+        fontSizeMax: 1.25,
+        ratioMin: 1.125,
+        ratioMax: 1.2,
+        screenMin: 20,
+        screenMax: 96,
+        unit: "rem",
+        prefix: "",
+      },
+      values: {
+        xs: [-2, 1.6],
+        sm: [-1, 1.6],
+        base: [0, 1.6],
+        lg: [1, 1.6],
+        xl: [2, 1.2],
+        "2xl": [3, 1.2],
+        "3xl": [4, 1.2],
+        "4xl": [5, 1.1],
+        "5xl": [6, 1.1],
+        "6xl": [7, 1.1],
+        "7xl": [8, 1],
+        "8xl": [9, 1],
+        "9xl": [10, 1],
+      },
+    }),
+  ],
+};
