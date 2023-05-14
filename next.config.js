a9955ea27e61eb018d91eaf61db5d307f5d8b85a
Anibal Santos <anibalsantosgo@gmail.com>
2023-05-14 20:59:17 +0200

    feat(rw): rw

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/** @type {import('next').NextConfig} */
-
 const rewritesJson = require("./src/config/rewrites.json");
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es", "it"],
+  },
   async rewrites() {
     return [
       ...rewritesJson,
