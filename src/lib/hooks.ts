5567d6ee4e8eda7022ddd1a1792a3f8a97dcb068
Anibal Santos <anibalsantosgo@gmail.com>
2023-05-15 17:29:28 +0200

    fix(broken): lint

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -2,19 +2,8 @@
 
 import { useEffect, useState } from "react";
 
-import { usePathname } from "next/navigation";
-
-import type { Locale } from "@/i18n";
-
 export const useMounted = () => {
   const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
   return mounted;
-};
-
-export const useGetLocale = () => {
-  let pathname = usePathname();
-  if (!pathname) pathname = "/";
-  const segments = pathname.split("/");
-  return { locale: segments[1] as Locale, pathname, segments };
 };
