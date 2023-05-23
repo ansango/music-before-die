f406e9875148af50baf570fcf0c0a74ac95ffb4f
Anibal Santos <anibalsantosgo@gmail.com>
2023-05-23 21:24:30 +0200

    refactor(list): components

--- src/app/artistas/(root)/letra/[letter]/page.tsx
+++ src/app/artistas/(root)/letra/[letter]/page.tsx
@@ -1,30 +1,9 @@
-import type { FC } from "react";
-
-import Link from "next/link";
-
+import { GreyCard, GreyCardList } from "@/components";
 import { getArtists, getArtistsByLetter, matchArtistByLetter } from "@/lib";
 
 import type { Artists } from "../../../../../../tina/__generated__/types";
 
 const letters = "abcdefghijklmnopqrstuvwxyz".split("");
-
-type ArtistLetterProps = { artists: Array<Artists> };
-
-const ArtistsByLetter: FC<ArtistLetterProps> = ({ artists }) => {
-  return (
-    <section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {artists?.map((artist) => (
-        <Link
-          key={artist._sys?.filename}
-          href={`/artistas/${artist._sys?.filename}`}
-          className="p-4 bg-base-200 link link-hover underline-offset-4"
-        >
-          {artist.name}
-        </Link>
-      ))}
-    </section>
-  );
-};
 
 type PageProps = {
   params: {
@@ -34,12 +13,17 @@ type PageProps = {
 
 export default async function Page({ params: { letter } }: PageProps) {
   const artists = (await getArtistsByLetter(letter)) as Array<Artists>;
-
   return (
-    <>
+    <article>
       <h1 className="text-4xl font-bold">{letter}</h1>
-      <ArtistsByLetter artists={artists} />
-    </>
+      <GreyCardList>
+        {artists?.map((artist) => (
+          <GreyCard key={artist._sys?.filename} href={`/artistas/${artist._sys?.filename}`}>
+            {artist.name}
+          </GreyCard>
+        ))}
+      </GreyCardList>
+    </article>
   );
 }
 
